e71b0b4311202c86134a6bf6ce4ef7ca30158e4a
shadow <uc_shadow@yahoo.com>
2015-09-28 11:55:56 +0300

    db exchange

--- cars.js
+++ cars.js
@@ -12,12 +12,24 @@ Router.route('/game/:name', {
     return Cars.findOne({ name: currentCar });
   }
 });
+Router.route('getInput');
+
+var countdown = new ReactiveCountdown();
+  countdown.start(function() {
+  console.log('done');
+});
+
+var available = "yes";
 
 if (Meteor.isClient) {
+
 
   Template.try.helpers({
     cars: function(){
       return Cars.find({});
+    },
+    timer: function(){
+      return available;
     }
   });
 
@@ -25,8 +37,36 @@ if (Meteor.isClient) {
     'click .car_container': function(event){
       event.preventDefault();
       var divId = this._id;
+      //$("#" + this.name).text("Available: no");
+      Router.go("gameScreen", {name: this.name});
     }
-  })
+  });
+
+  Template.gameScreen.rendered = function(){
+     $('body').keypress(function(event){
+      var pressed = event.key;
+      Input.update({_id: 'Wrk4kPcZfck3LvMit'}, { key: pressed });
+      var active_now = Input.findOne().key
+      setTimeout(
+      function(){
+        Input.update({_id: 'Wrk4kPcZfck3LvMit'}, { key: "nothing pressed!" });
+      }, 500);
+     });
+    };
+
+  Template.getInput.helpers({
+    input: function(){
+      var no_err = Input.findOne();
+      return no_err && no_err.key;  // && for error check
+    }
+  });
+
+  Template.gameScreen.helpers({
+    obj: function(){
+      var no_err = Input.findOne();
+      return no_err && no_err.key;  // && for error check
+    }
+  });
 
 }
 
@@ -40,3 +80,4 @@ if (Meteor.isServer) {
 
 
 Cars = new Meteor.Collection('cars');
+Input = new Meteor.Collection('input');
